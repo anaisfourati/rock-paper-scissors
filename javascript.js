2bd8dfb675cade2a0494b9b0fc50b129774730e2
anaisfourati <103164186+anaisfourati@users.noreply.github.com>
2024-06-11 20:18:05 +0200

    Add round logic and score incrementation.

--- javascript.js
+++ javascript.js
@@ -9,40 +9,53 @@
 // Create a function that randomly returns rock, paper or scissor
 
 const myArray = ['rock', 'paper', 'scissor'];
+let humanScore = 0;
+let computerScore = 0;
 
-function getComputerChoice() {
-    return myArray[Math.floor(Math.random() * myArray.length)];
-    // Explanation
-    // const randomNumber = Math.random();
-    // console.log(`Random Number = ${randomNumber}`);
+function playRound() {
+    let humanChoice = getHumanChoice().toLowerCase();
+    let computerChoice = getComputerChoice().toLowerCase();
 
-    // const arrayLength = myArray.length;
-    // console.log(`Array Length = ${arrayLength}`);
+    if (humanChoice === computerChoice) {
+        console.log("That's a draw! Try again...");
+    } 
 
-    // const multiplicationResult = randomNumber * arrayLength
-    // console.log(`Multiplication result = ${multiplicationResult}`);
-
-    // const roundedNumber = Math.floor(multiplicationResult);
-    // console.log(`Rounded Number = ${roundedNumber}`);
-    // return myArray[roundedNumber];
-}
-
-// Create a function that prompts the user to enter rock, paper or scissor and returns what the user entered
-
-function getHumanChoice() {
-
-    let userChoice = prompt("Choose between rock, paper or scissor!");
-
-    if (userChoice === 'rock') {
-        return 'rock';
-    } else if (userChoice === 'paper') {
-        return 'paper';
-    } else if (userChoice === 'scissor') {
-        return 'scissor';
+    if (humanChoice === 'rock' && computerChoice === 'scissor') {
+        score(true);
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        score(true);
+    } else if (humanChoice === 'scissor' && computerChoice === 'paper') {
+        score(true);
+    }  else {
+        score(false);
     }
-    
+
+    console.log(computerChoice);
+    console.log(humanScore);
+
+    // Functions that playRound needs to run
+    function getHumanChoice() {
+        let userChoice = prompt("Choose between rock, paper or scissor!");
+        return userChoice
+    }
+
+    function getComputerChoice() {
+        return myArray[Math.floor(Math.random() * myArray.length)];
+    }
+
+    function score(humanWon) {
+        if (humanWon) {
+            console.log(`You won! ${humanChoice} beats ${computerChoice}!`);
+            humanScore++;
+        } else {
+            console.log(`You lost... ${computerChoice} beats ${humanChoice}!`);
+            computerScore++;
+        }
+    }
 }
 
-console.log(getHumanChoice());
 
+
+
+console.log(playRound());
 
